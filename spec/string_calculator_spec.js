4f86fc0e108e1c3a4d88d852c05a978572af4cc3
Rama Mahmoud <if25b318@technikum-wien.at>
2026-06-04 17:17:16 +0200

    Initial commit: project structure

---
+++ spec/string_calculator_spec.js
@@ -0,0 +1,13 @@
+describe('String Calculator', function() {
+	beforeEach(function() {
+		calculator = new StringCalculator();
+	});
+
+	it('should return 0 for an empty string', function() {
+		expect(calculator.add('')).toEqual(0);
+	});
+
+	it('should return 1 for string 1', function() {
+		expect(calculator.add('1')).toEqual(1);
+	});
+});
---
+++ src/string_calculator.js
@@ -0,0 +1,10 @@
+// StringCalculator.js
+function StringCalculator() {}
+StringCalculator.prototype.add = function(string_numbers) {
+	
+    if (string_numbers === '') {
+        return 0;
+    }
+
+    return parseInt(string_numbers);
+};
