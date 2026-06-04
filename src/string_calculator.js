842348aa772e7cf225da2e594019990f20edd33d
Rama Mahmoud <if25b318@technikum-wien.at>
2026-06-04 17:38:11 +0200

    Green: split and sum numbers

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -2,9 +2,12 @@
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
 	
-    if (string_numbers === '') {
-        return 0;
-    }
+    if (string_numbers === '') return 0;
 
-    return parseInt(string_numbers);
+    return string_numbers
+        .split(',')
+        .reduce(function(sum, n) {
+            return sum + parseInt(n);
+        }, 0);
+        
 };
