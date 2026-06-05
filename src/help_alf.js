238b6dcc8f1c9f438ef4c585ff8245baf0be838b
Rama Mahmoud <if25b318@technikum-wien.at>
2026-06-05 22:58:23 +0200

    Green: find spaceship position on map

--- src/help_alf.js
+++ src/help_alf.js
@@ -5,4 +5,16 @@ HelpAlf.prototype.findSpaceship = function(map) {
     if(map === '')
     return 'Spaceship lost forever.';
     
+    var rows = map.split('\n');
+    
+    for (var y = 0; y < rows.length; y++) {
+        var cols = rows[y].split(' ');
+        for (var x = 0; x < cols.length; x++) {
+            if (cols[x] === 'X') {
+                return [x, rows.length - 1 - y];
+            }
+        }
+    }
+
+    
 };
