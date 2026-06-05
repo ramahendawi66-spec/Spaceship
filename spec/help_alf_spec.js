c34e67af52494458581e6e1610b3308ec7af2641
Rama Mahmoud <if25b318@technikum-wien.at>
2026-06-05 22:49:19 +0200

    Red: add test for finding spaceship in middle

--- spec/help_alf_spec.js
+++ spec/help_alf_spec.js
@@ -6,4 +6,10 @@ describe('Help ALF', function() {
     it('should return "Spaceship lost forever." for empty string', function() {
         expect(alf.findSpaceship('')).toEqual('Spaceship lost forever.');
     });
+
+    it('should find spaceship in the middle', function() {
+        expect(alf.findSpaceship('. . .\n. X .\n. . .')).toEqual([1, 1]);
+    }
+    
+    )
 });
